7ef98b438a1146ea83fe8f3997a34a0d447de8b3
Taylor Harwin <Taylor@Taylors-iMac.domain>
2016-05-05 22:11:27 -0700

    add regexp

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,32 @@
-const _ = require('lodash');
+const lodash = require('lodash')
+
+const mockData = require('./mockData');
+
+const isFormulaVariable = new RegExp(/\[(.*?)\]/);
+const isOperator = new RegExp(/\-|\*|\+|\//);
+const isValue = new RegExp(/[-.0-9]/);
+const isPartOfFormula = new RegExp(isFormulaVariable.source + "|" + isOperator.source + "|" + isValue.source);
 
 
 function Recipe(recipe){
 	if (!recipe || !recipe.formula){
 		throw new Error('requires a recipe with a formula');
 	}
+
 	this.formula = recipe.formula;
-	console.log(this.formula);
+
+	var formulaVars = this.formula.match(isPartOfFormula);
+	console.log(formulaVars);
+
+
+
+
 	
 }
 
-Recipe.prototype.
 
 
 module.exports = Recipe;
+
+
+var recipe = new Recipe(mockData);
